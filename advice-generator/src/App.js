21800119b183a19ea554cf473c3729690cefc44c
Ismail ‎ <ellouismail@gmail.com>
2022-07-27 15:21:23 +0100

    added the advice-geenrator web app

---
+++ advice-generator/src/App.js
@@ -0,0 +1,45 @@
+import React, {useState, useEffect, useMemo} from 'react';
+
+import './App.scss';
+import {images} from './images';
+
+function App() {
+  const [quote, setQuote] = useState(null);
+  const [quoteId, setQuoteId] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetchAdvice();
+  }, [])
+  
+  const fetchAdvice = async () => {
+    setLoading(true);
+    const { slip } = await (await fetch("https://api.adviceslip.com/advice")).json();
+    setLoading(false);
+    
+    setQuote(slip.advice);
+    setQuoteId(slip.id);
+  }
+
+  return (
+    <main className="wrapper">
+      <div className="content">
+        <h3 className='content__title'>ADVICE #{quoteId}</h3>
+        { 
+          !loading ? 
+          (
+            <p className="content__quote">"{quote}"</p>
+          )
+          :
+          <div className="loading" />
+        }
+        <div className="pattern-divider"/>
+      </div>
+      <button className="dice" onClick={fetchAdvice} disabled={loading? true : false}>
+        <img src={images.diceIcon} alt="Dice icon"/>
+      </button>
+    </main>
+  );
+}
+
+export default App;
---
+++ advice-generator/src/images/index.js
@@ -0,0 +1,9 @@
+import dice from './icon-dice.svg';
+import desktopPatternDevider from './pattern-divider-desktop.svg';
+import mobilePatternDevider from './pattern-divider-mobile.svg';
+
+export const images = {
+    diceIcon: dice,
+    desktopPatternDevider,
+    mobilePatternDevider
+}
